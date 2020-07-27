1ba6fce5d827ab9efd1cfcda67201d8063cba1a4
Matheus D Queiroz <mdqueiroz12@gmail.com>
2020-07-27 13:56:12 +0000

    adicionado 1041

--- Programacao/1041.c
+++ Programacao/1041.c
@@ -7,7 +7,25 @@ int main(){
 	
 	scanf("%f %f", &x, &y);
 	
-	
+	if(x > 0 && y > 0){
+        printf("Q1\n");
+    }else{
+        if(y > 0 && x < 0){
+            printf("Q2\n");
+        }else{
+            if(y < 0 && x < 0){
+                printf("Q3\n");
+            }else{
+                if(x > 0 && y < 0){
+                    printf("Q4\n");
+                }else{
+                    if(x == 0 && y == 0){
+                        printf("Origem\n");
+                    }
+                }
+            }
+        }
+    }
 	
 	return 0;
 }
